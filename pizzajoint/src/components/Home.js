9ffd2a592f6334c3cf9bf2626c46ef4c6f9ea9cc
Py-Ashraful <ashrafulislam180@gmail.com>
2022-02-12 04:00:15 +0600

    check motion

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <div className="home container">
-      <h2>
+      <motion.h2 animate={{ rotate: 360 }} transition={{ duration: 2 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+        <button>Create Your Pizza</button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
